1c105b703acd10f04591375aae9885e0624ba7f3
devonjerothe <devonjerothe@gmail.com>
2021-08-22 20:09:57 -0400

    Token validation and security

--- utils/oauth.js
+++ utils/oauth.js
@@ -1,19 +1,19 @@
-
+const jwt = require('jsonwebtoken');
 require('dotenv').config()
 
-exports.getToken = (req, res) => {
+exports.getToken = (req, res, next) => {
     const bearerHeader = req.headers['authorization'];
     if (typeof bearerHeader !== 'undefined') {
         const bearerToken = bearerHeader.split(' ')[0];
         req.token = bearerToken;
-        next();
+        verifyToken(req, res, next);
     }
     else {
-        res.status(403).send("Invalid Token")
+        res.status(401).send("Invalid Token")
     }
 }
 
-exports.verifyToken = (req, res) => {
+function verifyToken(req, res, next) {
     jwt.verify(req.token, process.env.TOKEN_SECRET, function(err, _authData) {
         if (err){
             res.status(403).send(err)
